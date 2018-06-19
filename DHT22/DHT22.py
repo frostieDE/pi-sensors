ecaa7a89594b06c9890c24671dea5075bd12b64c
Marcel Marnitz <marcelmarnitz@googlemail.com>
2018-06-19 23:03:14 +0200

    Improvements

--- DHT22/DHT22.py
+++ DHT22/DHT22.py
@@ -2,6 +2,7 @@
 
 import Adafruit_DHT
 import argparse
+import json
 import logging
 import os
 import signal
@@ -33,7 +34,7 @@ signal.signal(signal.SIGTERM, handler_stop_signals)
 ##################################################
 # CONFIGURATION
 ##################################################
-def config(verbosity, disable_influx, test_mode):
+def config(verbosity, disable_mqtt, test_mode):
     global caFile
 
     logger = logging.getLogger()
@@ -69,57 +70,71 @@ def config(verbosity, disable_influx, test_mode):
         logger.error('You must specify a MQTT topic');
         quit(1);
 
-    if disable_influx is not True:
-
+    if disable_mqtt is not True:
         if MQTT_HOST_ENV in os.environ:
             mqtt_host = os.environ[MQTT_HOST_ENV]
         else:
-            logger.warning("Use default influx host: localhost");
+            logger.warning("Use default MQTT host: localhost");
             mqtt_host = 'localhost'
 
         if MQTT_PORT_ENV in os.environ:
             mqtt_port = os.environ[MQTT_PORT_ENV]
         else:
-            logger.warning("Use default influx port: 8086");
-            mqtt_port = 8086
+            logger.warning("Use default MQTT port: 1883");
+            mqtt_port = 1883
 
         if MQTT_USER_ENV in os.environ:
             mqtt_user = os.environ[MQTT_USER_ENV]
         else:
-            logger.warning("Use default influx user: root");
+            logger.warning("Use default MQTT user: root");
             mqtt_user = 'root'
 
         if MQTT_PASS_ENV in os.environ:
             mqtt_password = os.environ[MQTT_PASS_ENV]
         else:
-            logger.warning("Use default influx password: root");
+            logger.warning("Use default MQTT password: root");
             mqtt_password = 'root'
 
         if MQTT_TLS_ENV in os.environ and os.environ[MQTT_TLS_ENV] == "True":
+            logger.debug("TLS enabled for MQTT")
             mqtt_tls = True
+
+            if MQTT_PORT_ENV not in os.environ:
+                mqtt_port = 8883
+                logger.warning("Use default MQTT port: 8883 (TLS)")
         else:
+            logger.debug("TLS disabled for MQTT")
             mqtt_tls = False
 
         client = mqtt.Client();
 
         if mqtt_tls == True:
+            logger.debug("Set CA certificated for MQTT client");
             client.tls_set(caFile);
 
+        logger.debug("Enable logger for MQTT client");
         client.enable_logger(logger)
+
+        logger.debug("Set username and password for MQTT client");
         client.username_pw_set(mqtt_user, mqtt_password)
 
         try:
+            logger.debug("Connect to MQTT server")
             client.connect(mqtt_host, mqtt_port)
         except:
             logger.error("Failed to connect to MQTT server");
             quit(2);
     else:
+        logger.debug("MQTT client disabled")
         client = None
 
     if test_mode is True:
+        logger.debug("Run in test mode")
         sensor = None
     else:
         sensor = Adafruit_DHT.DHT22
+
+    logger.debug("Configuration finished")
 
     return (logger, topic, pin, sensor, client)
 
@@ -142,10 +157,16 @@ def loop(logger, topic, pin, sensor = None, mqtt = None):
         logger.debug("Temperature: %s" % temperature)
         logger.debug("Humidity: %s" % humidity)
 
+        json_value = {
+                    'temperature': temperature,
+                    'humidity': humidity
+                }
+
+        logger.debug(json_value)
+
         if mqtt is not None:
             try:
-                mqtt.publish(topic + "/temperature", temperature)
-                mqtt.publish(topic + "/humidity", humidity)
+                mqtt.publish(topic, json.dumps(json_value))
             except:
                 logger.error("Publishing to MQTT server failed");
 
@@ -161,7 +182,7 @@ def loop(logger, topic, pin, sensor = None, mqtt = None):
 # ENTRY POINT
 ##################################################
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Read DHT22 sensor and log to InfluxDB")
+    parser = argparse.ArgumentParser(description="Read DHT22 sensor and log to MQTT")
     parser.add_argument('--verbose', '-v', action='count')
     parser.add_argument('--disable-mqtt', action='store_true')
     parser.add_argument('--test-mode', action='store_true')
